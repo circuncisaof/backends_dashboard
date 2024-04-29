e86cbeebff109e81d4e266ba49b67d33fab4ef30
fabio circuncisao <fabio.circuncisao@aditek.com.br>
2024-04-29 08:15:44 -0300

    project init

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,22 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 
+const PORTS = 3000;
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+  const app = await NestFactory.create(AppModule, {
+    cors: true,
+  });
+  app.setGlobalPrefix('api/v1');
+  const options = {
+    origin: '',
+    allowedHeaders: 'Content-Type, Accept',
+    credentials: true,
+    methods: 'GET,PUT,PATCH,POST,DELETE,OPTIONS',
+    optionsSuccessStatus: 200,
+    header: 'Access-Control-Allow-Origin: *',
+  };
+
+  app.enableCors(options);
+  await app.listen(PORTS);
 }
 bootstrap();
